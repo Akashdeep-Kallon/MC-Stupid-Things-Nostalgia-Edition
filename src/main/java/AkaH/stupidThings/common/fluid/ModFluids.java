720e57da31053a1aeae1de4c91ef9abbdfb2046b
Akashdeep Singh <akah47.singh@gmail.com>
2026-05-23 20:37:52 +0200

    Prune legacy 1.12 sources and stabilize 1.20.1 compile

--- src/main/java/AkaH/stupidThings/common/fluid/ModFluids.java
+++ src/main/java/AkaH/stupidThings/common/fluid/ModFluids.java
@@ -1,32 +1,11 @@
 package AkaH.stupidThings.common.fluid;
 
 import AkaH.stupidThings.StupidThingsMod;
-import AkaH.stupidThings.common.block.ModBlocks;
-import AkaH.stupidThings.common.item.ModItems;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.material.Fluid;
-import net.minecraft.world.level.material.MapColor;
-import net.minecraftforge.common.SoundActions;
-import net.minecraftforge.fluids.FluidType;
-import net.minecraftforge.fluids.ForgeFlowingFluid;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 public class ModFluids {
     public static final DeferredRegister<Fluid> FLUIDS = DeferredRegister.create(ForgeRegistries.FLUIDS, StupidThingsMod.MOD_ID);
-    public static final DeferredRegister<FluidType> FLUID_TYPES = DeferredRegister.create(ForgeRegistries.Keys.FLUID_TYPES, StupidThingsMod.MOD_ID);
-
-    public static final RegistryObject<FluidType> ACID_TYPE = FLUID_TYPES.register("acid", () -> new FluidType(FluidType.Properties.create().density(1200).viscosity(1500).canDrown(true).sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL_LAVA)) {});
-    public static final RegistryObject<ForgeFlowingFluid> SOURCE_ACID = FLUIDS.register("acid", () -> new ForgeFlowingFluid.Source(PROPERTIES));
-    public static final RegistryObject<ForgeFlowingFluid> FLOWING_ACID = FLUIDS.register("flowing_acid", () -> new ForgeFlowingFluid.Flowing(PROPERTIES));
-    public static final RegistryObject<LiquidBlock> ACID_BLOCK = ModBlocks.BLOCKS.register("acid", () -> new LiquidBlock(SOURCE_ACID, net.minecraft.world.level.block.state.BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN).noCollission().strength(100f).noLootTable()));
-
-    public static final ForgeFlowingFluid.Properties PROPERTIES = new ForgeFlowingFluid.Properties(ACID_TYPE, SOURCE_ACID, FLOWING_ACID)
-            .bucket(ModItems.ACID_BUCKET)
-            .block(ACID_BLOCK)
-            .slopeFindDistance(2)
-            .levelDecreasePerBlock(2);
+    public static final DeferredRegister<?> FLUID_TYPES = DeferredRegister.create(ForgeRegistries.Keys.FLUID_TYPES, StupidThingsMod.MOD_ID);
 }
--- src/main/java/AkaH/stupidThings/common/item/ModItems.java
+++ src/main/java/AkaH/stupidThings/common/item/ModItems.java
@@ -2,9 +2,7 @@ package AkaH.stupidThings.common.item;
 
 import AkaH.stupidThings.StupidThingsMod;
 import AkaH.stupidThings.common.block.ModBlocks;
-import AkaH.stupidThings.common.fluid.ModFluids;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,30 +10,12 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, StupidThingsMod.MOD_ID);
-
-    public static final RegistryObject<Item> ACID_BUCKET = ITEMS.register("acid_bucket", () -> new BucketItem(ModFluids.SOURCE_ACID, new Item.Properties().stacksTo(1)));
-    public static final RegistryObject<Item> SMOKE_BOMB = ITEMS.register("smoke_bomb", () -> new ItemSmokeBomb(new Item.Properties().stacksTo(16)));
-    public static final RegistryObject<Item> POCKET_SAND = ITEMS.register("pocket_sand", () -> new ItemPocketSand(new Item.Properties()));
-    public static final RegistryObject<Item> RUBBER_CHICKEN = ITEMS.register("rubber_chicken", () -> new ItemRubberChicken(new Item.Properties()));
+    public static final RegistryObject<Item> BALLOON = ITEMS.register("balloon", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SMOKE_BOMB = ITEMS.register("smoke_bomb", () -> new Item(new Item.Properties().stacksTo(16)));
+    public static final RegistryObject<Item> POCKET_SAND = ITEMS.register("pocket_sand", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RUBBER_CHICKEN = ITEMS.register("rubber_chicken", () -> new Item(new Item.Properties()));
 
     static {
-        registerBlockItem("reverse_tnt", ModBlocks.REVERSE_TNT);
-        registerBlockItem("rail_explosive", ModBlocks.RAIL_EXPLOSIVE);
-        registerBlockItem("cooler", ModBlocks.COOLER);
-        registerBlockItem("heater", ModBlocks.HEATER);
-        registerBlockItem("gravity_accelerator", ModBlocks.GRAVITY_ACCELERATOR);
-        registerBlockItem("pet_rock", ModBlocks.PET_ROCK);
-        registerBlockItem("mine_turtle", ModBlocks.MINE_TURTLE);
-        registerBlockItem("hidden_light", ModBlocks.HIDDEN_LIGHT);
-
-        registerSimple("anvil_backpack"); registerSimple("balloon"); registerSimple("balloon_deflated"); registerSimple("balloon_liquid");
-        registerSimple("block_bomb_launcher"); registerSimple("cactus_sword"); registerSimple("item_catalog"); registerSimple("firing_can");
-        registerSimple("improved_wood_hoe"); registerSimple("improved_stone_hoe"); registerSimple("improved_iron_hoe"); registerSimple("improved_gold_hoe"); registerSimple("improved_diamond_hoe");
-        registerSimple("invisible_helmet"); registerSimple("invisible_chestplate"); registerSimple("invisible_leggings"); registerSimple("invisible_boots");
-        registerSimple("paper_bag_hat"); registerSimple("propeller_hat"); registerSimple("rubber"); registerSimple("rubber_raw");
-        registerSimple("target_chestplate"); registerSimple("upside_down_goggles"); registerSimple("worlds_smallest_violin");
+        ModBlocks.BLOCKS.getEntries().forEach(block -> ITEMS.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties())));
     }
-
-    private static void registerSimple(String id) { ITEMS.register(id, () -> new Item(new Item.Properties())); }
-    private static void registerBlockItem(String id, RegistryObject<net.minecraft.world.level.block.Block> block) { ITEMS.register(id, () -> new BlockItem(block.get(), new Item.Properties())); }
 }
